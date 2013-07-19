a76d58e9f05561630e70d40c12731627df7dcc5a
Justin Kramer <jkkramer@gmail.com>
2013-07-19 11:32:31 -0400

    cljx for tests

--- project.clj
+++ project.clj
@@ -6,11 +6,18 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1843"]]
   :source-paths ["src/cljx"]
+  :test-paths ["target/test-classes"]
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/classes"
                    :rules :clj}
                   {:source-paths ["src/cljx"]
                    :output-path "target/classes"
+                   :rules :cljs}
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test-classes"
+                   :rules :clj}
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test-classes"
                    :rules :cljs}]}
   :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.0.5"]
                                   [com.keminglabs/cljx "0.3.0"]]
