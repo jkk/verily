d2b311baf65c30a88198412654c5d50db2e58042
Justin Kramer <jkkramer@gmail.com>
2013-07-19 12:20:42 -0400

    0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jkkramer/verily "0.5.0-SNAPSHOT"
+(defproject jkkramer/verily "0.5.0"
   :description "Map validation library"
   :url "https://github.com/jkk/verily"
   :license {:name "Eclipse Public License"
