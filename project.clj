b46f58b2abbfa4f8cfac89cc61ec2340e137340a
Justin Kramer <jkkramer@gmail.com>
2013-07-19 09:31:50 -0400

    0.5.0-SNAPSHOT, clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject jkkramer/verily "0.4.4"
+(defproject jkkramer/verily "0.5.0-SNAPSHOT"
   :description "Map validation library"
   :url "https://github.com/jkk/verily"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
